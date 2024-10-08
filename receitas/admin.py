fda151609ccaabff701e0659dafdfb926a55a93d
drussilla <drussilla@ezcodeit.com>
2024-10-08 19:26:27 -0300

    Novo app Pessoas, e foreinKey para receitas

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Receita
 
 
-admin.site.register(Receita)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ("id", "nome_receita", "categoria")
+    list_display_links = ("id", "nome_receita")
+    search_fields = ("nome_receita",)
+    # Nesse campo, precisa passar um param tipo tupla, por isso a vírgula após o param passado.
+    list_filter = ("categoria", )
+    list_per_page = 2
+
+
+admin.site.register(Receita, ListandoReceitas)
--- receitas/models.py
+++ receitas/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from pessoas.models import Pessoa
 
 
 class Receita(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.SET_DEFAULT, null=True, default=None)
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
